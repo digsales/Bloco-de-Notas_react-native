89fd5a8e855580a66d5add68fc8a33ff0525a836
_digsales <diogodobu@gmail.com>
2022-11-17 12:59:17 -0300

    Adding TaskList screen and putting font

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import TaskList from "./src/screens/TaskList";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Lato: require("./assets/fonts/Lato.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return <TaskList />;
 }
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -6,23 +6,51 @@ import {
   StyleSheet,
   StatusBar,
 } from "react-native";
+
+import commomStyles from "../commomStyles";
 import todayImage from "../../assets/imgs/today.jpg";
+// import * as Font from "expo-font";
+
 import moment from "moment";
 import "moment/locale/pt-br";
 
 export default class TaskList extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     fontsLoaded: false,
+  //   };
+  // }
+
+  // async loadFonts() {
+  //   await Font.loadAsync({
+  //     Lato: require("../../assets/fonts/Lato.ttf"),
+  //   });
+  //   this.setState({ fontsLoaded: true });
+  // }
+
+  // componentDidMount() {
+  //   this.loadFonts();
+  // }
+
+  Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   render() {
     const today = moment().locale("pt-br").format("ddd, D [de] MMMM");
     return (
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
           <View style={styles.titleBar}>
-            <Text style={[styles.textoCria, { fontSize: 40 }]}>Hoje</Text>
-            <Text style={styles.textoCria}>{today}</Text>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{this.Capitalize(today)}</Text>
           </View>
         </ImageBackground>
         <View style={styles.taskList}>
-          <Text>TaskList</Text>
+          <Text>Tarefa #01</Text>
+          <Text>Tarefa #02</Text>
+          <Text>Tarefa #03</Text>
         </View>
         <StatusBar
           barStyle={"light-content"}
@@ -45,14 +73,21 @@ const styles = StyleSheet.create({
   taskList: {
     flex: 7,
   },
-  textoCria: {
-    color: "white",
-    fontSize: 30,
-  },
   titleBar: {
     flex: 1,
     justifyContent: "flex-end",
-    paddingLeft: 16,
-    paddingBottom: 16,
+    padding: 20,
+  },
+  title: {
+    fontFamily: commomStyles.fontFamily,
+    color: commomStyles.colors.secondary,
+    fontSize: 50,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commomStyles.fontFamily,
+    color: commomStyles.colors.secondary,
+    fontSize: 20,
+    marginBottom: 10,
   },
 });
