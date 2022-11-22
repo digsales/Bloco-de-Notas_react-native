7d545c4399a4d9d509f8ca891613f2d6d4f381d4
_digsales <diogodobu@gmail.com>
2022-11-22 10:00:56 -0300

    Task style v1

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default (props) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{props.desc}</Text>
       <Text>{String(props.estimateAt)}</Text>
       <Text>{String(props.doneAt)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderColor: "#aaa",
+    borderBottomWidth: 1,
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+});
