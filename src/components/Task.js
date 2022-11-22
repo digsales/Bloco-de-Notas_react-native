1016ade0045ad72cf9ab166372619b2d024b8aaa
_digsales <diogodobu@gmail.com>
2022-11-22 10:17:53 -0300

    Task style v2

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import commomStyles from "../commomStyles";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default (props) => {
   return (
     <View style={styles.container}>
-      <Text>{props.desc}</Text>
-      <Text>{String(props.estimateAt)}</Text>
-      <Text>{String(props.doneAt)}</Text>
+      <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
+      <View>
+        <Text>{props.desc}</Text>
+        <Text>{String(props.estimateAt)}</Text>
+      </View>
     </View>
   );
 };
+
+function getCheckView(doneAt) {
+  if (doneAt != null) {
+    return (
+      <View style={styles.done}>
+        <FontAwesome name="check" size={15} color="white" />
+      </View>
+    );
+  } else {
+    return <View style={styles.pending}></View>;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +34,27 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: "center",
     paddingVertical: 10,
+  },
+  checkContainer: {
+    width: "20%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pending: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: "#555",
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: "#555",
+    backgroundColor: "#1E90FF",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
