83679562e43d876e0c0f09dbef0d78a306a96a8f
_digsales <diogodobu@gmail.com>
2022-12-22 19:24:11 -0300

    Updating modal screen

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -4,7 +4,9 @@ import {
   View,
   StyleSheet,
   TouchableWithoutFeedback,
+  Text,
 } from "react-native";
+import commomStyles from "../commomStyles";
 
 export default class AddTask extends Component {
   render() {
@@ -16,7 +18,11 @@ export default class AddTask extends Component {
         animationType="slide"
       >
         <TouchableWithoutFeedback onPress={this.props.onCancel}>
-          <View style={styles.background}></View>
+          <View style={styles.background}>
+            <View style={styles.container}>
+              <Text style={styles.header}>Nova Tarefa</Text>
+            </View>
+          </View>
         </TouchableWithoutFeedback>
       </Modal>
     );
@@ -27,5 +33,19 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: "rgba(0, 0, 0, 0.7)",
+    justifyContent: "center",
+  },
+  container: {
+    flex: 1 / 2,
+    backgroundColor: "white",
+    marginHorizontal: 30,
+  },
+  header: {
+    fontFamily: commomStyles.fontFamily,
+    fontSize: 20,
+    backgroundColor: commomStyles.colors.today,
+    color: commomStyles.colors.secondary,
+    textAlign: "center",
+    padding: 5,
   },
 });
